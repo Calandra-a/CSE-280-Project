25f57dcc50134b06f30bb66c0a82cde8bbb15629
nmmcgivern <nmmcgivern@oakland.edu>
2017-02-10 13:47:40 -0500

    Add files via upload
    Changed setPopUp() to use a new ControllerManager
    closeWindow() added

--- src/controllers/ControllerManager.java
+++ src/controllers/ControllerManager.java
@@ -5,6 +5,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import scenes.logIn.Controller;
 
 /**
  * Created by Noah McGivern on 2/4/2017.
@@ -33,6 +34,13 @@ public class ControllerManager {
     }
 
     /**
+     * Closes the window (Stage).
+     */
+    public void closeWindow() {
+        stage.close();
+    }
+
+    /**
      * Switches to the logInScene
      *
      * @throws Exception IOException: file wasn't found, etc
@@ -44,10 +52,6 @@ public class ControllerManager {
     public void testScene() throws Exception {
         setScene("../scenes/test/Scene.fxml");
     }
-    /*public void registerScene() throws Exception{
-
-        setScene("../scenes/register/Scene.fxml");
-    }*/
 
     /**
      * Switches to the designated Scene.
@@ -55,7 +59,7 @@ public class ControllerManager {
      * @param name The pathname of the .fxml to load.
      * @throws Exception IOException: file wasn't found, etc
      */
-    public void setScene(String name) throws Exception {
+    private void setScene(String name) throws Exception {
         //Get the scene and load it
         fxmlLoader = new FXMLLoader(getClass().getResource(name));
         root = fxmlLoader.load();
@@ -64,9 +68,12 @@ public class ControllerManager {
         ((BudgetMeController) fxmlLoader.getController()).setControllerManager(this);
         //Finally, change to the new scene
         stage.setScene(new Scene(root));
-
-        stage.setResizable(true);
     }
+
+    public void registerPopUp() throws Exception {
+        setPopUp("../scenes/test/Scene.fxml");
+    }
+
     public boolean testPopUp() throws Exception {
         //Returns the value of pressed from the test Controller, by calling setPopUp
         //to create a new scene in a new window, then using its FXMLLoader to get
@@ -81,14 +88,15 @@ public class ControllerManager {
      * @return The FXMLLoader of the specified .fxml file, after the pop up window is closed.
      * @throws Exception IOException: file wasn't found, etc
      */
-    public FXMLLoader setPopUp(String name) throws Exception {
-        //Cwwreate a new stage, and change the modality so it must be dealt with
+    private FXMLLoader setPopUp(String name) throws Exception {
+        //Create a new stage, and change the modality so it must be dealt with
         Stage popUp = new Stage();
         popUp.initModality(Modality.APPLICATION_MODAL);
+        ControllerManager controllerManager = new ControllerManager(popUp);
         //Do the same as in setScene, but with this popUp stage
         FXMLLoader tempLoader = new FXMLLoader(getClass().getResource(name));
         Parent scene = tempLoader.load();
-        ((BudgetMeController) tempLoader.getController()).setControllerManager(this);
+        ((BudgetMeController) tempLoader.getController()).setControllerManager(controllerManager);
         popUp.setScene(new Scene(scene));
         popUp.showAndWait();
         //Wait until the window is dealt with (closed), then return the FXMLLoader
